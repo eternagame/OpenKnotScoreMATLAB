0dec3c6df743bfeefe0660f36a7decca95677fb7
Rhiju Das <rhiju@stanford.edu>
2023-06-15 17:05:21 -0700

    add options to allow clip of data, show truncated list and specification of Spearman/Pearson as corr_type

--- matlab/score/get_corr_coeff.m
+++ matlab/score/get_corr_coeff.m
@@ -1,16 +1,22 @@
-function [all_corr_coef, pkg_sort_idx] = get_corr_coeff( r, mfe_structure_map, good_idx, mfe_tags, BLANK_OUT3, BLANK_OUT5);
-% [all_corr_coef, pkg_sort_idx] = get_corr_coeff( r, mfe_structure_map, good_idx, mfe_tags, BLANK_OUT3, BLANK_OUT5);
+function [all_corr_coef, pkg_sort_idx] = get_corr_coeff( r, structure_map, good_idx, structure_tags, BLANK_OUT3, BLANK_OUT5, corr_type, num_show, clip);
+% [all_corr_coef, pkg_sort_idx] = get_corr_coeff( r, structure_map, good_idx, structure_tags, BLANK_OUT3, BLANK_OUT5, corr_type, num_show, clip);
+%
+% Note that values are clipped so that negative values go to zero; 
+%  high values can be clipped to maximum specified by the 'clip' variable.
 %
 % Inputs
 %  r = [Ndesign x Nres x Ncond] Reactivity matrix. Assume last of Ncond has
 %  SHAPE data.
-%  mfe_structure_map = [Ndesign x Nres x Npackages] 0/1 map
+%  structure_map = [Ndesign x Nres x Npackages] 0/1 map
 %            of paired/unpaired for each predicted structure
 %  good_idx = [list of integers] index of designs to use for correlation
 %            coefficients
-%  mfe_tags = cell of string, name of each package
+%  structure_tags = cell of string, name of each package
 %  BLANK_OUT5 = gray out this number of 5' residues
 %  BLANK_OUT3 = gray out this number of 3' residues 
+%  corr_type  = correlation coefficient type (default:'Pearson')
+%  num_show   = number of top algorithms to show (default: all)
+%  clip       = maximum value to clip at [Default no clipping]
 %
 % Outputs
 %   all_corr_coef = [Npackages] correlation coefficients for each package
@@ -19,19 +25,25 @@ function [all_corr_coef, pkg_sort_idx] = get_corr_coeff( r, mfe_structure_map, g
 %
 % (C) R. Das, HHMI/Stanford University 2023.
 
+if ~exist( 'corr_type','var') | length(corr_type)==0; corr_type = 'Pearson'; end;
+if ~exist( 'num_show','var') | num_show == 0; num_show = length(structure_tags); end;
+if ~exist( 'clip','var') | clip == 0; clip=Inf; end;
 idx_shape_condition = size(r, 3);
 
 which_pos = (BLANK_OUT5+1):(size(r,2)-BLANK_OUT3);
-r_subset = max(r(good_idx, which_pos, idx_shape_condition),0);
-for n = 1:size( mfe_structure_map, 3)    
-    mfe_structure_map_subset = mfe_structure_map(good_idx,which_pos,n);
+r_subset = min(max(r(good_idx, which_pos, idx_shape_condition),0),clip);
+for n = 1:size( structure_map, 3)    
+    structure_map_subset = structure_map(good_idx,which_pos,n);
     gp = find (~isnan(r_subset(:)));
-    R = corrcoef( r_subset(gp), mfe_structure_map_subset(gp));
-    all_corr_coef(n) = R(1,2);
+%    R = corrcoef( r_subset(gp), structure_map_subset(gp));
+    all_corr_coef(n) = corr(r_subset(gp), structure_map_subset(gp),'Type',corr_type);
 end
 
 all_corr_coef( isnan( all_corr_coef) ) = -1;
-[~,pkg_sort_idx] = sort(all_corr_coef);
-for n = pkg_sort_idx(end:-1:1)
-    fprintf( '%7.4f %s\n',all_corr_coef(n),mfe_tags{n});
+[~,pkg_sort_idx] = sort(all_corr_coef,'descend');
+fprintf('Using correlation type %s',corr_type);
+if clip < Inf; fprintf('and maximum value (clip) of %8.3f',clip); end;
+fprintf('\n');
+for n = pkg_sort_idx(1:num_show)
+    fprintf( '%7.4f %s\n',all_corr_coef(n),structure_tags{n});
 end
