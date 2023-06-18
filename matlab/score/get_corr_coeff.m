cd11692f3da685247a030bc3864c8e3afd401863
Rhiju Das <rhiju@stanford.edu>
2023-06-17 19:50:59 -0700

    more graceful use of corr, agnostic to transposes of internal vectors. output best_corr_coeff

--- matlab/score/get_corr_coeff.m
+++ matlab/score/get_corr_coeff.m
@@ -1,4 +1,4 @@
-function [all_corr_coef, pkg_sort_idx] = get_corr_coeff( r, structure_map, good_idx, structure_tags, BLANK_OUT3, BLANK_OUT5, corr_type, num_show, clip);
+function [all_corr_coef, pkg_sort_idx, best_corr_coeff] = get_corr_coeff( r, structure_map, good_idx, structure_tags, BLANK_OUT3, BLANK_OUT5, corr_type, num_show, clip);
 % [all_corr_coef, pkg_sort_idx] = get_corr_coeff( r, structure_map, good_idx, structure_tags, BLANK_OUT3, BLANK_OUT5, corr_type, num_show, clip);
 %
 % Note that values are clipped so that negative values go to zero; 
@@ -28,22 +28,25 @@ function [all_corr_coef, pkg_sort_idx] = get_corr_coeff( r, structure_map, good_
 if ~exist( 'corr_type','var') | length(corr_type)==0; corr_type = 'Pearson'; end;
 if ~exist( 'num_show','var') | num_show == 0; num_show = length(structure_tags); end;
 if ~exist( 'clip','var') | clip == 0; clip=Inf; end;
-idx_shape_condition = size(r, 3);
 
+idx_shape_condition = size(r, 3);
 which_pos = (BLANK_OUT5+1):(size(r,2)-BLANK_OUT3);
 r_subset = min(max(r(good_idx, which_pos, idx_shape_condition),0),clip);
-for n = 1:size( structure_map, 3)    
+for n = 1:size( structure_map, 3)   
     structure_map_subset = structure_map(good_idx,which_pos,n);
     gp = find (~isnan(r_subset(:)));
-%    R = corrcoef( r_subset(gp), structure_map_subset(gp));
-    all_corr_coef(n) = corr(r_subset(gp), structure_map_subset(gp),'Type',corr_type);
+    r_subset_vals = r_subset(gp);
+    structure_map_subset_vals = structure_map_subset(gp);
+    %    R = corrcoef( r_subset(gp), structure_map_subset(gp));
+    all_corr_coef(n) = corr(r_subset_vals(:), structure_map_subset_vals(:),'Type',corr_type);
 end
 
 all_corr_coef( isnan( all_corr_coef) ) = -1;
 [~,pkg_sort_idx] = sort(all_corr_coef,'descend');
 fprintf('Using correlation type %s',corr_type);
-if clip < Inf; fprintf('and maximum value (clip) of %8.3f',clip); end;
+if clip < Inf; fprintf(' and maximum value (clip) of %8.3f',clip); end;
 fprintf('\n');
 for n = pkg_sort_idx(1:num_show)
     fprintf( '%7.4f %s\n',all_corr_coef(n),structure_tags{n});
 end
+best_corr_coeff = max(all_corr_coef);
