3a5ad8367efec12933e6318a1ea6768039d5ff3d
Rhiju Das <rhiju@stanford.edu>
2023-08-06 07:52:34 -0700

    revisit detect_tert_structure

--- matlab/score/detect_tert_structure_v2.m
+++ matlab/score/detect_tert_structure_v2.m
@@ -1,5 +1,5 @@
-function detect_tert_structure( sequences, r_norm, good_idx, BLANK_OUT5, BLANK_OUT3, structures_rnastructure, headers, structures, structures_shapeknots, r_norm_err, SHOW_PLOT  );
-% detect_tert_structure( sequences, r_norm, good_idx, BLANK_OUT5, BLANK_OUT3, structures_rnastructure, headers, structures,  structures_shapeknots, r_norm_err, SHOW_PLOT  );
+function protected_loop2_score = detect_tert_structure_v2( sequences, r_norm, good_idx, BLANK_OUT5, BLANK_OUT3, openknot_info_structs, headers, r_norm_err, SHOW_PLOT   );
+% detect_tert_structure_v2( sequences, r_norm, good_idx, BLANK_OUT5, BLANK_OUT3, structure_sets_extended, structure_tags_extended, headers, r_norm_err, SHOW_PLOT   );
 %
 % (C) R. Das, HHMI/Stanford 2023
 
@@ -9,33 +9,34 @@ if SHOW_PLOT; set(figure(15),'color','white','position',[100 500 1200 250]); clf
 N = length(sequences{1});
 which_pos = [BLANK_OUT5+1:N-BLANK_OUT3];
 rprof_err = [];
-for i = good_idx'
+protected_loop2_scores = [];
+for q = 1:length(good_idx)
+    i = good_idx(q);
     sequence  = sequences{i};
-    structure = structures{i};
-    structure_rnastructure = structures_rnastructure{i};
-    structure_pk = '';
-    if i <= length(structures_shapeknots); structure_pk = structures_shapeknots{i}; end
-    if length(structure_pk) == 0; structure_pk = repmat('.',length(sequence),1); end;
+    structures = openknot_info_structs{i}.best_fit.structures;
+    % find non-empty structure:
+    for m = 1:length(structures)
+        structure_full = openknot_info_structs{i}.best_fit.structures{m};
+        structure_tag = openknot_info_structs{i}.best_fit.tags{m};
+        if contains(structure_full, '('); break; end;
+    end
+    structure = remove_pk( structure_full );
     rprof = r_norm(i,:);
     if exist('r_norm_err','var'); rprof_err = r_norm_err(i,:); end;
-    protected_loop = intersect(intersect( find(rprof<0.25),strfind(structure_rnastructure,'.')),which_pos);
+    protected_loop = intersect(intersect( find(rprof<0.25),strfind(structure,'.')),which_pos);
     protected_loop2 = intersect(protected_loop,protected_loop+1);
     protected_loop2 = union(protected_loop2,protected_loop2-1);
-    exposed_pair = intersect(setdiff( find(rprof>0.25),strfind(structure_rnastructure,'.')),which_pos);
-
+    exposed_pair = intersect(setdiff( find(rprof>0.25),strfind(structure,'.')),which_pos);
+    protected_loop2_score(q) = 100*length(protected_loop2)/length(which_pos);
     if SHOW_PLOT
         plot( rprof, 'k','linew',2 ); hold on;
         plot([0 length(sequence)],[0 0],'k')
         plot([0 length(sequence)],0.25*[1 1],'k:')
-        for m = 1:length(structure)
-            text(m,-0.2,structure_rnastructure(m),'horizontalalign','center');
-            text(m,-0.4,structure_pk(m),'horizontalalign','center');
-            text(m,-0.6,structure(m),'horizontalalign','center');
-            text(m,-0.8,sequence(m),'horizontalalign','center');
+        for m = 1:length(structure_full)
+            text(m,-0.2,structure_full(m),'horizontalalign','center');
+            text(m,-0.4,sequence(m),'horizontalalign','center');
         end
-        text(length(structure)+1,-0.2,'SHAPE-RNAstructure','horizontalalign','left')
-        text(length(structure)+1,-0.4,'SHAPE-knots','horizontalalign','left');
-        text(length(structure)+1,-0.6,'Eterna','horizontalalign','left');
+        text(length(structure)+1,-0.2,structure_tag,'horizontalalign','left','interpreter','none')
         plot_res = which_pos; clr = [0 0 0]; plot_symbols(rprof,rprof_err,plot_res, clr);
         plot_res = protected_loop; clr = [0.2 0.3 0.2]; plot_symbols(rprof,rprof_err,plot_res, clr);
         plot_res = protected_loop2;clr = [0 0.7 0]; plot_symbols(rprof,rprof_err,plot_res, clr);
@@ -47,6 +48,13 @@ for i = good_idx'
         if length(good_idx)>1 & i ~= good_idx(end); pause;end;
     end
 end
+
+
+%%%%%%%
+function structure = remove_pk( structure_full );
+structure=structure_full;
+pk_pos = setdiff( [1:length(structure)], [strfind(structure,'.'),strfind(structure,'('),strfind(structure,')')] );
+structure(pk_pos) = '.';
 
 %%%%%%%
 function plot_symbols(rprof,rprof_err,plot_res,clr); 
