1ea8be33ef99367f3213400be7ab1df30bfc3a06
Rhiju Das <rhiju@stanford.edu>
2023-08-29 17:16:23 -0700

    allow read in of multiple structure_sets CSV files

--- matlab/readers/read_structure_sets_csv_file.m
+++ matlab/readers/read_structure_sets_csv_file.m
@@ -21,35 +21,60 @@ function [x,structure_tags, structure_sets,structure_map,found_structure_idx] = 
 warning('off');
 if ~exist('sanitize_structures','var') sanitize_structures = 1; end;
 tic
-x = readtable(structure_sets_csv_file);
-structure_tags = {}; structure_sets = {}; count = 0;
-fprintf('\n');
-for n = 1:length(x.Properties.VariableNames);
-    tag = x.Properties.VariableNames{n};
-    structures = table2cell(x(:,n));
-    if contains(tag,'eternafold_threshknot_'); continue; end;
-    if contains(tag,'sequence'); continue; end;
-    is_structure_col = 1;
-    for i = 1:size(x,1) 
-        if strcmp(structures{i},'ERR'); structures{i}=repmat('.',1,length(structures{1})); end;
-        if ~ischar(structures{i}) | (~contains(structures{i},'.') & ~contains(structures{i},'(') & ~contains(structures{i},'x') )
-            is_structure_col = 0;
-            break;
+structure_tags = {}; structure_sets = {}; sequences = {};
+if iscell( structure_sets_csv_file )
+    structure_sets_csv_files = structure_sets_csv_file;
+else
+    structure_sets_csv_files = {structure_sets_csv_file};
+end
+
+fprintf('Reading in tables\n');
+for q = 1:length(structure_sets_csv_files)
+    structure_sets_csv_file = structure_sets_csv_files{q};
+    fprintf('Reading table: %s\n',structure_sets_csv_file);
+    x = readtable(structure_sets_csv_file);
+    sequence_col = find(strcmp(x.Properties.VariableNames,'sequence'));
+    %assert(~isempty(sequence_col));
+    sequences = [sequences;table2cell(x(:,sequence_col))];
+
+    for n = 1:length(x.Properties.VariableNames);
+        tag = x.Properties.VariableNames{n};
+        structures = table2cell(x(:,n));
+        if contains(tag,'eternafold_threshknot_'); continue; end;
+        if contains(tag,'sequence'); continue; end;
+        if strcmp(tag,'id'); continue; end;
+        is_structure_col = 1;
+        for i = 1:size(x,1)
+            if strcmp(structures{i},'ERR'); structures{i}=repmat('.',1,length(structures{1})); end;
+            if ~ischar(structures{i}) | (~contains(structures{i},'.') & ~contains(structures{i},'(') & ~contains(structures{i},'x') )
+                is_structure_col = 0;
+                break;
+            end
         end
-    end
-    if ~is_structure_col; continue; end;
-    count = count + 1;
-    structure_tags{count} = strip(strrep(strrep(tag,'__mfe',''),'_mfe',''),'_');
-    structure_sets{count} = structures;
-    if sanitize_structures
-        fprintf( 'Sanitizing %d structures for %s...\n',size(x,1),structure_tags{count});
-        for i = 1:size(x,1) % loop over designs
-            structure_sets{count}{i} = sanitize_structure( structure_sets{count}{i} );
+        if ~is_structure_col; continue; end;
+        structure_tag = strip(strrep(strrep(tag,'__mfe',''),'_mfe',''),'_');
+        idx = find((strcmp(structure_tags,structure_tag)));
+        if isempty( idx )
+            structure_tags = [structure_tags,{structure_tag}];
+            idx = length(structure_tags);
+            structure_sets{idx} = {};
         end
+        structure_sets{idx} = [structure_sets{idx};structures];
     end
 end
 fprintf('\n');
 toc
+
+if sanitize_structures
+    tic
+    for count = 1:length(structure_sets)
+        fprintf( 'Sanitizing %d structures for %s...\n',length(structure_sets{count}),structure_tags{count});
+        for i = 1:length(structure_sets{count}) % loop over designs
+            structure_sets{count}{i} = sanitize_structure( structure_sets{count}{i} );
+        end
+    end
+    toc
+end
 
 % may need to do a reordering if sequence order in structure file does not
 % match sequences 
@@ -58,9 +83,8 @@ if exist('ordered_sequences','var') & length( ordered_sequences ) > 0
     tic
     fprintf( 'Matching into ordered sequences...\n')
     assert(length(unique(ordered_sequences))==length(ordered_sequences)); % check uniqueness
-    sequence_col = find(strcmp(x.Properties.VariableNames,'sequence'));
-    assert(~isempty(sequence_col));
-    sequences = table2cell(x(:,sequence_col));
+    sequences = strrep(sequences,'T','U');
+    ordered_sequences = strrep(ordered_sequences,'T','U');
     assert(length(unique(sequences))==length(sequences)); % check uniqueness
     count = 0;
     structure_sets_from_csv = structure_sets;
